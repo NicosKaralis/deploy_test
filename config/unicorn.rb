fdfc6656c16fc57dbeec2ef23bf1620a101fee4c
Nicos Karalis <nicoskaralis@me.com>
2013-02-18 20:28:13 -0300

    Adicionado newrelic para unicorn 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,8 +7,3 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.blog.sock"
 worker_processes 2
 timeout 30
-
-# Ensure the agent is started using Unicorn
-# This is needed when using Unicorn and preload_app is not set to true.
-# See http://support.newrelic.com/kb/troubleshooting/unicorn-no-data
-::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
